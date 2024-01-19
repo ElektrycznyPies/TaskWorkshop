f4b450afe7d02d0f4954a3949c5383754f6a7f90
ElektrycznyPies <zen.zukowski@gmail.com>
2024-01-20 00:17:49 +0100

    Lists Options Menu. Displays tasks from file. Adds a new task.

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,7 +20,10 @@ public class TaskManager {
 
     public static void main(String[] args) {
 
-        String[] optionsMenu = new String[] {"add", "remove", "list", "exit"}; //Makes Options Menu array
+        int listing = 0;
+        readTasks(listing);
+
+        String[] optionsMenu = new String[]{"add", "remove", "list", "exit"}; //Makes Options Menu array
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Please select an option: ");
@@ -35,14 +39,18 @@ public class TaskManager {
             if (input.equals("exit")) {
                 break;
             } else if (input.equals("list")) {
-                readTasks();
+                listing = 1; readTasks(listing);
+            } else if (input.equals("add")) {
+                addTask();
+            } else if (input.equals("remove")) {
+                removeTask();
             } else {
                 System.out.println("Unknown command, please select a valid option.");
             }
         }
     }
 
-    public static void readTasks() {
+    public static void readTasks(int listing) {
 
         int tasksNumber = 0;
 
@@ -52,9 +60,9 @@ public class TaskManager {
                 tasksNumber++;              //finds number of lines (tasks) in file
                 scan.nextLine();
             }
-        }   catch (FileNotFoundException ex) {
-                System.out.println("Problem z plikiem.");
-            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Problem z plikiem.");
+        }
 
         String[][] tasks = new String[tasksNumber][];   //makes array for tasks based on no. of tasks
 
@@ -64,13 +72,49 @@ public class TaskManager {
                     String taskLine = scan.nextLine();
                     String[] row = taskLine.split(", ");
                     tasks[i] = row;                     //loads tasks to array
-                    System.out.println(i+1 + " : " + Arrays.toString(tasks[i]));
+                    if (listing == 1) { System.out.println(i + 1 + " : " + Arrays.toString(tasks[i])); }
                 }
             }
-        }   catch (FileNotFoundException ex) {
+        } catch (FileNotFoundException ex) {
             System.out.println("Problem z plikiem.");
         }
     }
-}
 
+    public static void addTask() {
+
+        int listing = 0;
+        readTasks(listing);
+
+        Scanner scan = new Scanner(System.in);              //user inputs new Task data
+        System.out.println("Enter task description: ");
+        String taskDescription = scan.nextLine();
+        System.out.println("Enter due date: ");
+        String taskDate = scan.nextLine();
+        String taskImp = "";
+        while (true) {
+            System.out.println("Is this task important? (true / false) ");
+            taskImp = scan.nextLine();
+            if (taskImp.equals("true") || taskImp.equals("false")) {
+                break;
+            } else {
+                System.out.println("Input only true or false");
+            }
+        }
+
+        File file = new File("tasks.csv");      //new Task is written to csv file
+        try (FileWriter writer = new FileWriter(file, true)) {
+            System.out.println(taskDescription + "  " + taskImp);
+            writer.append(taskDescription).append(", ").append(taskDate).append(", ").append(taskImp).append("\n"); //adds input as one row (one new task)
+        } catch (IOException ex) {
+                System.out.println("problem z plikiem");
+            }
+    }
+
+    public static void removeTask() {
+    }
+
+    public static void listTasks() {
+
+    }
+}
 
